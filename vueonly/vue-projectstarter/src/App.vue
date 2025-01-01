5335c65f7b6b3295c37319803cbee6418b76149c
hawi-me <hawiabdi41@gmail.com>
2025-01-01 23:01:54 +0400

    updats

---
+++ vueonly/vue-projectstarter/src/App.vue
@@ -0,0 +1,28 @@
+<script setup >
+import navbar from './components/navbar.vue';
+// import {ref} from 'vue';
+
+//   const name = ref('vue3');
+//   const age = ref('20');
+//   const task = ref(['vue3', 'vue2', 'vue1']);
+//   const addTask = () => {
+//     task.value.push(name.value);
+//     name.value = '';
+//   };
+  
+//   const newTask = ref('');
+
+  
+ 
+  
+
+
+</script>
+
+<template>
+<navbar></navbar>
+</template>
+
+<style scoped>
+
+</style>
